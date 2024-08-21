3078ef92e04d43c9fae66ee14ade57d7786812ae
Gaurav N Pawar <78693210+gaurav3014@users.noreply.github.com>
2024-08-21 11:01:23 +0530

    Update stable_diffusion.py

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 from PIL import Image
-from torch import autocast
 from diffusers import StableDiffusionPipeline
 import torch
 
@@ -16,19 +15,19 @@ prompt = st.text_input("Enter your prompt here:", "")
 if st.button("Generate"):
     if prompt:
         model_id = "CompVis/stable-diffusion-v1-4"
-        device = "cpu"
+        device = "cpu"  # Change to "cuda" if you have a GPU
 
         # Retrieve the token from environment variables
-        auth_token = st.secrets["API_TOKEN"]
+        auth_token = st.secrets.get("API_TOKEN")
         if not auth_token:
-            st.error("API token not found. Please set the HUGGINGFACEHUB_API_TOKEN environment variable.")
+            st.error("API token not found. Please set the API_TOKEN in Streamlit secrets.")
         else:
             # Load the model
-            pipe = StableDiffusionPipeline.from_pretrained(model_id, revision='fp16', torch_dtype=torch.float16, use_auth_token=auth_token)
+            pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=auth_token)
             pipe.to(device)
 
             # Generate the image
-            with autocast(device):
+            with torch.no_grad():  # No need for autocast on CPU
                 image = pipe(prompt, guidance_scale=8.5)["sample"][0]
             
             # Save and display the image
